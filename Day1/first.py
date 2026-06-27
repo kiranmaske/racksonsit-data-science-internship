96d074bfba58640f1023c97d83e443f405820ef0
Kiran Maske <kiranmaske043@gmail.com>
2026-06-27 14:55:16 +0530

    Day 1 - Python Basics
    Uploaded Day 1 internship Python exercises and practice programs.

---
+++ Day1/first.py
@@ -0,0 +1,69 @@
+# num = 10
+# sum = num + 10
+# print(sum)
+
+# num = 2
+# print(num** 3)
+
+# x,y,z = 34,45,56
+# print(x+y+z)
+
+# num = 78; num = num = 22; print(num)
+
+# name = "Sandesh"
+# print(type(name))
+# print(name[0])
+
+#Escspe Statements
+# print("Hello\nWorld")
+# print("Hello\rWorld")
+# print("Hello\\World")
+
+#Printing Statements with Format
+# name = "Sandesh"
+# age = 20
+# print(name,age)
+# print("My name is",name,"and my age is",age)
+# print("My name is %s and My age is %d "%(name,age))
+
+# print("My name is {} and My age is {}".format(name,age))
+# print("My name is {0} and My age is {1}".format(name,age))
+# print("My name is {x} and My age is {y}".format(x=name,y=age))
+# print(name*3)
+
+# Implicit Type Conversion
+# num1 = 45
+# num2 = 56.78
+# num3 = num1 + num2
+# print(num3)
+
+# num1 = 45
+# num2 = 56.78
+# num3 = num1 + int(num2)
+# print(num3)
+
+# a = input("Enter a number: ")
+# print(a)
+
+# a = int(input("Enter a number: "))
+# b = a + 10
+# print(b)
+
+# a = 6
+# b = 7
+
+# print("A = ",b)
+# print("B = ",a)
+
+# a = a+b
+# b = a-b
+# a = a-b
+# print("A = ",a)
+# print("B = ",b) 
+
+# a = a*b
+# b = a/b
+# a = a/b
+# print("A = ",a)
+# print("B = ",b)
+
